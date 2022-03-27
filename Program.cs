285391432efbd591c33f7bb6ece0e5b05d2393bd
Putseyeu <putseyeu.a.d@gmail.com>
2022-03-27 20:37:21 +0200

    step1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkArrayText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "В прошлом посте мы делали условия на основе иерархии типов";
+            string[] textArray = text.Split(' ');
+
+            foreach (var word in textArray)
+            {
+                Console.WriteLine($"{word}");
+            }
+        }
+    }
+}
